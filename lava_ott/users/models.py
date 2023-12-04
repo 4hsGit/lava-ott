a231934f3740f0e346242c6d6e4d2dd10d99d93f
bibin <bibinbabyanthony@gmail.com>
2023-12-04 13:29:04 +0530

    User model created and migration

--- lava_ott/users/models.py
+++ lava_ott/users/models.py
@@ -1,8 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractBaseUser):
+class User(AbstractUser):
+    GENDER_CHOICES = [('M', 'Male'), ('F', 'Female'), ('T', 'Transgender'), ('O', 'Others')]
+
     mobile_number = models.CharField(max_length=25, unique=True)
+
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    dob = models.DateField(blank=True, null=True)
+
     is_subscriber = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
     image = models.ImageField(upload_to='user_image/', blank=True, null=True)
