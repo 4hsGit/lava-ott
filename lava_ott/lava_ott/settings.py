1c62014d5ae5f370f6b3835f317188cc8336f5c7
bibin <bibinbabyanthony@gmail.com>
2023-12-04 13:21:44 +0530

    settings updated

--- lava_ott/lava_ott/settings.py
+++ lava_ott/lava_ott/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     # new
     'rest_framework',
     # local apps
-    'users',
-    'videos'
+    'users.apps.UsersConfig',
+    'videos.apps.VideosConfig'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
