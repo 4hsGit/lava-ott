0e83296b1ff9d5c06c3af4d10895a787795a4293
bibin <bibinbabyanthony@gmail.com>
2023-12-04 14:27:50 +0530

    urls added for user APIs

--- lava_ott/lava_ott/urls.py
+++ lava_ott/lava_ott/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+url_prefix = 'api/'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(f'{url_prefix}users/', include('users.urls')),
+    path(f'{url_prefix}videos/', include('videos.urls'))
 ]
--- lava_ott/users/urls.py
+++ lava_ott/users/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
+from .views import *
 
 
 urlpatterns = [
-    path()
+    path('login/', AdminLoginView.as_view()),
+    path('status/', StatusView.as_view()),
+    path('logout/', LogoutView.as_view()),
+
+    path('otp-send/', OTPSendView.as_view()),
+    path('otp-validate/', OTPVerifyView.as_view()),
+
+    path('registration/', UserRegistrationView.as_view()),
+    path('list/', UserListView.as_view()),
+    path('profile/', UserProfileView.as_view()),
 ]
