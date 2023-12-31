092439c26f2673467c0ba51659f4bd6c270e8f49
bibin <bibinbabyanthony@gmail.com>
2023-12-31 11:44:14 +0530

    test commit

--- lava_ott/lava_ott/settings/testing.py
+++ lava_ott/lava_ott/settings/testing.py
@@ -2,9 +2,8 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['lavaott-1-be4120441dfc.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 import django_heroku
-import dj_database_url
 
 django_heroku.settings(locals())
