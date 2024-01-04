fdc96d39bc6581f0e5aa81e4d5c98f266a370141
bibin <bibinbabyanthony@gmail.com>
2024-01-05 00:29:08 +0530

    mobile number serializer update

--- users/serializers.py
+++ users/serializers.py
@@ -8,11 +8,11 @@ class LoginSerializer(serializers.Serializer):
 
 
 class OTPSendSerializer(serializers.Serializer):
-    mobile_number = serializers.CharField(min_length=10, max_length=10)
+    mobile_number = serializers.IntegerField(min_value=100000000, max_length=9999999999)
 
 
 class OTPVerfySerializer(serializers.Serializer):
-    mobile_number = serializers.CharField(max_length=10, min_length=10)
+    mobile_number = serializers.IntegerField(min_value=100000000, max_length=9999999999)
     otp = serializers.IntegerField(min_value=100000, max_value=999999,
                                    error_messages={'max_value': 'OTP must contain 6 digits.',
                                                    'min_value': 'OTP must contain 6 digits.'})
@@ -20,14 +20,14 @@ class OTPVerfySerializer(serializers.Serializer):
 
 
 class RegistrationOTPVerfySerializer(serializers.Serializer):
-    mobile_number = serializers.CharField(max_length=10, min_length=10)
+    mobile_number = serializers.IntegerField(min_value=100000000, max_length=9999999999)
     otp = serializers.IntegerField(min_value=100000, max_value=999999,
                                    error_messages={'max_value': 'OTP must contain 6 digits.',
                                                    'min_value': 'OTP must contain 6 digits.'})
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    mobile_number = serializers.CharField(max_length=10, min_length=10)
+    mobile_number = serializers.IntegerField(min_value=100000000, max_length=9999999999)
     otp = serializers.IntegerField(min_value=100000, max_value=999999,
                                    error_messages={'max_value': 'OTP must contain 6 digits.',
                                                    'min_value': 'OTP must contain 6 digits.'})
