fd215ced7f4373b00c2f882b5ebea0f1db60cee2
bibin <bibinbabyanthony@gmail.com>
2024-01-09 01:05:29 +0530

    CORS_ALLOW_HEADERS test

--- lava_ott/settings/testing.py
+++ lava_ott/settings/testing.py
@@ -6,17 +6,17 @@ ALLOWED_HOSTS = ['lavaott-979ac37aaaa6.herokuapp.com']
 
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    'http://*',
-    'https://*',
-    'http://localhost:3000'
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://*',
+#     'https://*',
+#     'http://localhost:3000'
+# ]
 CORS_ALLOW_HEADERS = [
     # 'access-control-allow-headers',
     # 'access-control-allow-methods',
     # 'access-control-allow-origin',
-    'authorization',
-    'content-type',
+    # 'authorization',
+    # 'content-type',
     'xauth',
 ]
 
