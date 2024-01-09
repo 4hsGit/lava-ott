1fe7647c1716088f83fa70505b6c8609f5e73b60
bibin <bibinbabyanthony@gmail.com>
2024-01-09 12:56:18 +0530

    video list update for test

--- videos/utils.py
+++ videos/utils.py
@@ -70,7 +70,7 @@ def subscription_exists(user):
 def get_video(video, app=None):
     if app is True:
         trailer = 'https://s3.ap-south-1.amazonaws.com/4handstudio.in/videos/web+1920x1080.mp4'
-        file = 'https://s3.ap-south-1.amazonaws.com/4handstudio.in/videos/web+1920x1080.mp4'
+        file = 'https://s3.ap-south-1.amazonaws.com/4handstudio.in/videos/lava+test+1min.mp4'
     else:
         file = video.file.url if video.file else ''
         trailer = video.trailer.url if video.trailer else ''
