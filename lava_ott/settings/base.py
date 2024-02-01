eda8afa78a74215d5438d49b9ead1fc50a1c4f67
bibin <bibinbabyanthony@gmail.com>
2024-02-02 01:05:21 +0530

    test

--- lava_ott/settings/base.py
+++ lava_ott/settings/base.py
@@ -188,10 +188,10 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 # For serving static files directly from S3
 AWS_S3_URL_PROTOCOL = 'https:'
-AWS_S3_USE_SSL = True
-AWS_S3_VERIFY = True
+# AWS_S3_USE_SSL = True
+# AWS_S3_VERIFY = True
 
-AWS_DEFAULT_ACL = None
+# AWS_DEFAULT_ACL = None
 
 # Static and media file configuration
 STATIC_URL = f'{AWS_S3_URL_PROTOCOL}//{AWS_S3_CUSTOM_DOMAIN}/static/'
