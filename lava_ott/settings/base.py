2052653ae990426c8422d069f5620a1019016cd8
bibin <bibinbabyanthony@gmail.com>
2024-03-13 00:05:17 +0530

    db change

--- lava_ott/settings/base.py
+++ lava_ott/settings/base.py
@@ -92,10 +92,21 @@ WSGI_APPLICATION = 'lava_ott.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lava_ott_db',
+        'USER': 'root',
+        'PASSWORD': 'uV3AsmqkSD9pydkL',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
