ed4939d421e439108e9ef18db422ff2e07189010
bibin <bibinbabyanthony@gmail.com>
2024-03-13 23:48:49 +0530

    production db configured

--- lava_ott/settings/base.py
+++ lava_ott/settings/base.py
@@ -92,23 +92,13 @@ WSGI_APPLICATION = 'lava_ott.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'lava_ott_db',
-        'USER': 'root',
-        'PASSWORD': 'uV3AsmqkSD9pydkL',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- lava_ott/settings/production.py
+++ lava_ott/settings/production.py
@@ -1,3 +1,14 @@
 from .base import *
 
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lava_ott_db',
+        'USER': 'root',
+        'PASSWORD': 'uV3AsmqkSD9pydkL',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
