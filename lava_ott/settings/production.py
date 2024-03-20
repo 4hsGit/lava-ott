037f9a20b57637e96dd7cccef1396d4b566592d5
bibin <bibinbabyanthony@gmail.com>
2024-03-20 21:54:36 +0530

    db conf commented

--- lava_ott/settings/production.py
+++ lava_ott/settings/production.py
@@ -2,13 +2,13 @@ from .base import *
 
 DEBUG = False
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'lava_ott_db',
-        'USER': 'root',
-        'PASSWORD': 'uV3AsmqkSD9pydkL',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'lava_ott_db',
+#         'USER': 'root',
+#         'PASSWORD': 'uV3AsmqkSD9pydkL',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
