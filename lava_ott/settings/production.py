178b01d11ae7b153bd8e992b1dbb1eb3a938f010
bibin <bibinbabyanthony@gmail.com>
2024-03-20 21:57:46 +0530

    db conf postgres added

--- lava_ott/settings/production.py
+++ lava_ott/settings/production.py
@@ -2,13 +2,13 @@ from .base import *
 
 DEBUG = False
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'lava_ott_db',
-#         'USER': 'root',
-#         'PASSWORD': 'uV3AsmqkSD9pydkL',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mylava',
+        'USER': 'mylavauser',
+        'PASSWORD': 'mylava@2024',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
