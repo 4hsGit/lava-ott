b5e039294f0ab9b96e8db5dfd57591d0cee706c3
bibin <bibinbabyanthony@gmail.com>
2024-07-08 01:23:38 +0530

    test

--- videos/admin.py
+++ videos/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import Video, Order, SubscriptionPlan, Carousel
 
-admin.site.register(Video)
-admin.site.register(Order)
-admin.site.register(SubscriptionPlan)
-admin.site.register(Carousel)
+# admin.site.register(Video)
+# admin.site.register(Order)
+# admin.site.register(SubscriptionPlan)
+# admin.site.register(Carousel)
