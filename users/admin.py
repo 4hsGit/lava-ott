00135d264efe12fadfba6575dbdfe598430684b4
bibin <bibinbabyanthony@gmail.com>
2024-09-16 15:58:00 +0530

    model added in admin

--- users/admin.py
+++ users/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 from .models import User
 from .models import Project
+from .models import DeletedUser
 
 
 admin.site.register(User)
 admin.site.register(Project)
+admin.site.register(DeletedUser)
